97e02d3c621e329afc3e5fe793de3e580aaee585
Yokiijay <yokiijay@gmail.com>
2020-05-29 04:01:58 +0800

    2020-05-29 04:01:56

--- bin/newfile.js
+++ bin/newfile.js
@@ -1,14 +1,35 @@
 #!/usr/bin/env node
-const program = require('commander')
-const pkg = require('../package.json')
+// const program = require('commander')
+// const pkg = require('../package.json')
 
-program
-  .version(pkg.version)
-  .description(pkg.description)
-  .arguments('<filepath>')
-  .command('then [cmds...]', 'commands to run after created file')
-  .action((out, cmds)=>{
-    console.log( out, cmds )
+// program
+//   .version(pkg.version)
+//   .description(pkg.description)
+//   .arguments('<filepath>')
+//   .command('then [cmd]', 'commands to run after created file')
+//   .action((out, cmd)=>{
+//     console.log( out, cmd )
+//   })
+
+// program.parse(process.argv)
+
+
+const argv = require('yargs')
+  .usage(`
+🌞🌈 Do something after created file~
+
+Usage: 
+  $ newfile <filepath> then [cmds...]`)
+  .example('newfile src/index.js then cd')
+  .example('newfile src/index.js then yarn start')
+  .command('<filepath> then [cmd]', '', yargs=>{
+    console.log( yargs )
+  }, argv=>{
+    console.log( argv )
   })
+  .alias('h', 'help')
+  .alias('v', 'version')
+  .boolean(['then'])
+  .argv
 
-program.parse(process.argv)
+console.log( argv )
